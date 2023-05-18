9698f23b1da4981a1570c877d70529ed173639c0
camiloesan <camiloesan@gmail.com>
2023-05-18 12:03:09 -0600

    m

--- src/TestCloning.java
+++ src/TestCloning.java
@@ -1,11 +1,11 @@
 package src;
 
-public class TestCloning {
+public class TestCloning extends CloneFactory {
     public static void main(String[] args) {
         CloneFactory dogFactory = new CloneFactory();
 
         Dog myDog = new Dog("Blanco", "Grande");
-        System.out.println("Perro original: " + myDog + "; ID: " + System.identityHashCode(myDog));
+        System.out.println("Perro original: " + myDog.color + ", " + myDog.size + "; ID: " + System.identityHashCode(myDog));
 
         for (int i = 0; i < 5; i++) {
             Dog clonedDog = (Dog) dogFactory.getClone(myDog);
